3a0a8ebf863267ac3c62e23a00c670f7eb7f5c23
Lakin Wecker <lakin@structuredabstraction.com>
2022-04-15 08:56:19 -0600

    ALlowing port to be specified as well

--- modules/client/Env.py
+++ modules/client/Env.py
@@ -7,7 +7,7 @@ from modules.game.EngineTools import EngineTools
 class Env:
     def __init__(self, config: ConfigWrapper, token: Opt[str] = None):
         self.config = config
-        self.url = "{}://{}".format(self.config.server.protocol, self.config.server.domain)
+        self.url = "{}://{}:{}".format(self.config.server.protocol, self.config.server.domain, self.config.server.port)
         self.engineTools = EngineTools.new(self.config)
         if token is None:
             self.auth = self.config.auth.asdict()
